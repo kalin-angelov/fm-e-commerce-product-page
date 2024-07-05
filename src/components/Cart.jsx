ab7c38cb7254c776a6827dec4bc188f46beda18e
Kalizar <kalinangelov@abv.bg>
2024-07-05 17:54:43 +0300

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 import { Context } from "../context/Context";
 import Product from "./Product";
 
-const Cart = ( { cartHandler ,setCartHandler } ) => {
+const Cart = ( { cartOpen ,setCartOpen } ) => {
   const { quantity } = useContext(Context);
+  const cartRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutsideCart = (event) => {
+      if (cartRef.current && !cartRef.current.contains(event.target)) {
+        setCartOpen(false);
+      };
+    };
+
+    if (cartOpen) {
+      document.addEventListener("mousedown", handleClickOutsideCart)
+    };
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideCart);
+    };
+
+  },[cartOpen]);
   
   return (
-    <section className={cartHandler ? "cart-container-close" : "cart-container"}>
+    <section className={cartOpen ? "cart-container" : "cart-container-close"} ref={cartRef}>
       <h3>Cart</h3>
       <div className="cart-content">
         {quantity !== 0 ?
-          <Product cartHandler={cartHandler} setCartHandler={setCartHandler} />
+          <Product carOpen={cartOpen} setCartOpen={setCartOpen} />
           :
           <p className="message">Your cart is empty.</p>
         }
