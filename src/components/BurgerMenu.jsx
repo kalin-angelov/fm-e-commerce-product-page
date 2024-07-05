a2221fc5021a1f68e879412214bbf7662da683a2
Kalizar <kalinangelov@abv.bg>
2024-07-05 17:54:45 +0300

    Update BurgerMenu.jsx

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,9 +1,28 @@
-const BurgerMenu = ( { burgerHandler ,setBurgerHandler } ) => {
+import { useEffect, useRef } from "react";
+
+const BurgerMenu = ( { menuOpen ,setMenuOpen } ) => {
+    const menuRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setMenuOpen(false);
+            };
+        };
+
+        if (menuOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    },[menuOpen]);
 
     return (
-        <aside className={burgerHandler ? "burger-menu-close" : "burger-menu"} >
-            <nav className="navigation">
-                <figure onClick={() => setBurgerHandler(!burgerHandler)}>
+        <aside className={menuOpen ? "burger-menu" : "burger-menu-close"} >
+            <nav className="navigation" ref={menuRef}>
+                <figure onClick={() => setMenuOpen(!menuOpen)}>
                     <img src="/public/images/icon-close.svg" alt="X btn" />
                 </figure>
 
