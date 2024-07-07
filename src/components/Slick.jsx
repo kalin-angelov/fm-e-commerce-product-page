93a119fafa2b66c7138d4da296ffcb9a7d96e1fb
Kalizar <kalinangelov@abv.bg>
2024-07-07 15:29:05 +0300

    update

--- src/components/Slick.jsx
+++ src/components/Slick.jsx
@@ -64,16 +64,16 @@ const Slick = () => {
                         </figure>
                         <ul className="img-catalog">
                             <li  onClick={(e) => onFocus(e)}>
-                                <img className={imgNum == 1 ? "img-on" : null} id="1" src="/public/images/image-product-1-thumbnail.jpg" alt="product" />
+                                <img className={imgNum == 1 ? "img-on" : null} id="1" src="/images/image-product-1-thumbnail.jpg" alt="product" />
                             </li>
                             <li onClick={(e) => onFocus(e)}>
-                                <img className={imgNum == 2 ? "img-on" : null} id="2" src="/public/images/image-product-2-thumbnail.jpg" alt="product" />
+                                <img className={imgNum == 2 ? "img-on" : null} id="2" src="/images/image-product-2-thumbnail.jpg" alt="product" />
                             </li>
                             <li onClick={(e) => onFocus(e)}>
-                                <img className={imgNum == 3 ? "img-on" : null} id="3" src="/public/images/image-product-3-thumbnail.jpg" alt="product" />
+                                <img className={imgNum == 3 ? "img-on" : null} id="3" src="/images/image-product-3-thumbnail.jpg" alt="product" />
                             </li>
                             <li onClick={(e) => onFocus(e)}>
-                                <img className={imgNum == 4 ? "img-on" : null} id="4" src="/public/images/image-product-4-thumbnail.jpg" alt="product" />
+                                <img className={imgNum == 4 ? "img-on" : null} id="4" src="/images/image-product-4-thumbnail.jpg" alt="product" />
                             </li>
                         </ul>
                     </div>
