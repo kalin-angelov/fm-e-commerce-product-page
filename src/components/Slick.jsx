d7d90e7fc3de1642f8d2d32b13440c7bcce7b6b6
Kalizar <kalinangelov@abv.bg>
2024-07-09 11:19:54 +0300

    Update Slick.jsx

--- src/components/Slick.jsx
+++ src/components/Slick.jsx
@@ -31,29 +31,29 @@ const Slick = () => {
         <> {windowSize < 1024 ?
                 <div className="img-slider">
 
-                <div className="img-container">
-                    <figure style={{display: slideNumber === 1 ? "block": "none"}}>
-                        <img src="/images/image-product-1.jpg" alt="product img" />
-                    </figure>
-                    <figure style={{display: slideNumber === 2 ? "block": "none"}}>
-                        <img src="/images/image-product-2.jpg" alt="product img" />
-                    </figure>
-                    <figure style={{display: slideNumber === 3 ? "block": "none"}}>
-                        <img src="/images/image-product-3.jpg" alt="product img" />
-                    </figure>
-                    <figure style={{display: slideNumber === 4 ? "block": "none"}}>
-                        <img src="/images/image-product-4.jpg" alt="product img" />
-                    </figure>
-                </div>
+                    <div className="img-container">
+                        <figure style={{display: slideNumber === 1 ? "block": "none"}}>
+                            <img src="/images/image-product-1.jpg" alt="product img" />
+                        </figure>
+                        <figure style={{display: slideNumber === 2 ? "block": "none"}}>
+                            <img src="/images/image-product-2.jpg" alt="product img" />
+                        </figure>
+                        <figure style={{display: slideNumber === 3 ? "block": "none"}}>
+                            <img src="/images/image-product-3.jpg" alt="product img" />
+                        </figure>
+                        <figure style={{display: slideNumber === 4 ? "block": "none"}}>
+                            <img src="/images/image-product-4.jpg" alt="product img" />
+                        </figure>
+                    </div>
 
-                <div className="slider-btn">
-                    <button className="btn-container" id="previous" onClick={(e) => sliderControl(e)}>
-                        <img src="/images/icon-previous.svg" alt="arrow img" />
-                    </button>
-                    <button className="btn-container" id="next" onClick={(e) => sliderControl(e)}>
-                        <img src="/images/icon-next.svg" alt="arrow img" />
-                    </button>
-                </div>
+                    <div className="slider-btn">
+                        <button className="btn-container" id="previous" onClick={(e) => sliderControl(e)}>
+                            <img src="/images/icon-previous.svg" alt="arrow img" />
+                        </button>
+                        <button className="btn-container" id="next" onClick={(e) => sliderControl(e)}>
+                            <img src="/images/icon-next.svg" alt="arrow img" />
+                        </button>
+                    </div>
 
                 </div>
                 :
