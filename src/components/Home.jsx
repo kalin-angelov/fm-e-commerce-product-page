64d83a9dce77f7855e59961a0537668f7b5950c6
Kalizar <kalinangelov@abv.bg>
2024-07-09 11:19:51 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,30 +26,33 @@ const Home = () => {
 
     return (
         <main>
-            <Slick />
-           
-            <section className="main-page-container">
-                <h1>sneaker company</h1>
-                <h2>Fall Limited Edition Sneakers</h2>
-                <p className="summary">
-                    These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.
-                </p>
-                <div className="price-tag">
-                    <p className="discounted-price">$125.00 <span className="discount">50%</span></p>
-                    <p className="regular-price">$250.00</p>
-                </div>
-                <div className="buttons-container">
-                    <div className="quantity-btn button">
-                        <img src="/images/icon-minus.svg" alt="minus" onClick={() => quantityHandler("minus")} />
-                        {quantity}
-                        <img src="/images/icon-plus.svg" alt="plus" onClick={() => quantityHandler("plus")} />
+            <div className="main-container">
+                <Slick />
+            
+                <section className="summary-container">
+                    <h1>sneaker company</h1>
+                    <h2>Fall Limited Edition Sneakers</h2>
+                    <p className="summary">
+                        These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.
+                    </p>
+                    <div className="price-tag">
+                        <p className="discounted-price">$125.00 <span className="discount">50%</span></p>
+                        <p className="regular-price">$250.00</p>
                     </div>
-                    <button className="add-cart-btn button" onClick={onAddToCart}>
-                        <img src="/images/icon-cart.svg" alt="cart icon" />
-                        Add to cart
-                    </button>
-                </div>
-            </section>
+                    <div className="buttons-container">
+                        <div className="quantity-btn button">
+                            <img src="/images/icon-minus.svg" alt="minus" onClick={() => quantityHandler("minus")} />
+                            {quantity}
+                            <img src="/images/icon-plus.svg" alt="plus" onClick={() => quantityHandler("plus")} />
+                        </div>
+                        <button className="add-cart-btn button" onClick={onAddToCart}>
+                            <img src="/images/icon-cart.svg" alt="cart icon" />
+                            Add to cart
+                        </button>
+                    </div>
+                </section>
+            </div>
+           
         </main>
     );
 };
